5fc0ce7bc20f6691cb636221534692605f7d9c83
swaroop <swaroopiit@gmail.com>
2024-10-20 20:21:29 +0530

    solved some more problems

---
+++ 2. Problems/p1_tree_sum/p1_tree_sum.py
@@ -0,0 +1,29 @@
+def tree_sum(root):
+    pass
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        
+a = Node(3)
+b = Node(11)
+c = Node(4)
+d = Node(4)
+e = Node(-2)
+f = Node(1)
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#       3
+#    /    \
+#   11     4
+#  / \      \
+# 4   -2     1
+
+tree_sum(a) # -> 21
