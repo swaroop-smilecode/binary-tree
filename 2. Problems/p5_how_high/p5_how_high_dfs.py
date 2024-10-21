3d178ac3de7d75013b73503d59a945458b3eccac
swaroop <swaroopiit@gmail.com>
2024-10-21 14:33:55 +0530

    Solved some more problems

---
+++ 2. Problems/p5_how_high/p5_how_high_dfs.py
@@ -0,0 +1,35 @@
+
+
+def how_high(node):
+    if node is None:
+        return 0
+    return 1 + max(how_high(node.left), how_high(node.right))
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+root = a
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+print(how_high(root)) # -> 2
