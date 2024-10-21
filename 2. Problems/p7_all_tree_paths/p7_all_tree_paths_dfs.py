83a08b9fe4db7bd58fcadf77f90f6c8eded48464
swaroop <swaroopiit@gmail.com>
2024-10-21 19:52:28 +0530

    Solved some more problems

--- 2. Problems/p7_all_tree_paths/p7_all_tree_paths_dfs.py
+++ 2. Problems/p7_all_tree_paths/p7_all_tree_paths_dfs.py
@@ -1,3 +1,20 @@
+def all_tree_paths(root):
+  if root is None:
+    return []
+
+  if root.left is None and root.right is None:
+    return [[root.val]]
+    
+    left_path = all_tree_paths(root.left)
+    right_path = all_tree_paths(root.right)
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
 a = Node('a')
 b = Node('b')
 c = Node('c')
