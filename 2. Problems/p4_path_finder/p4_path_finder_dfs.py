66380240f58a178f68816d00324a477042027c83
swaroop <swaroopiit@gmail.com>
2024-10-21 11:20:43 +0530

    solved some more problems

--- 2. Problems/p4_path_finder/p4_path_finder_dfs.py
+++ 2. Problems/p4_path_finder/p4_path_finder_dfs.py
@@ -1,10 +1,21 @@
-path = []
 def path_finder(root, target):
-    if root is None:
-        pass
-    return 
-        
+  if root is None:
+    return None
+  
+  if root.val == target:
+    return [ root.val ]
+  
+  left_path = path_finder(root.left, target)
+  if left_path is not None:
+    return [ root.val, *left_path ]
+  
+  right_path = path_finder(root.right, target)
+  if right_path is not None:
+    return [ root.val, *right_path ]
+  
+  return None
 
+        
 class Node:
     def __init__(self, val):
         self.val = val
@@ -24,10 +35,11 @@ b.left = d
 b.right = e
 c.right = f
 
+root = a
 #      a
 #    /   \
 #   b     c
 #  / \     \
 # d   e     f
 
-path_finder(a, 'e') # -> [ 'a', 'b', 'e' ]
+print(path_finder(root, 'e')) # -> [ 'a', 'b', 'e' ]
