734f5d0e940083ce0a489a5efb67c3b21e2749e8
swaroop <swaroopiit@gmail.com>
2024-10-22 06:44:26 +0530

    Solved some more problems

--- 2. Problems/p7_all_tree_paths/p7_all_tree_paths_dfs.py
+++ 2. Problems/p7_all_tree_paths/p7_all_tree_paths_dfs.py
@@ -1,12 +1,29 @@
 def all_tree_paths(root):
+  paths = _all_tree_paths(root)
+  for path in paths:
+    path.reverse()
+  return paths
+
+def _all_tree_paths(root):
   if root is None:
     return []
 
   if root.left is None and root.right is None:
     return [[root.val]]
-    
-    left_path = all_tree_paths(root.left)
-    right_path = all_tree_paths(root.right)
+
+  all_paths = []
+  
+  left_sub_paths = _all_tree_paths(root.left)
+  for path in left_sub_paths:
+    path.append(root.val)
+    all_paths.append(path)
+
+  right_sub_paths = _all_tree_paths(root.right)
+  for path in right_sub_paths:
+    path.append(root.val)
+    all_paths.append(path)
+
+  return all_paths
 
 
 class Node:
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# What is a tree?
+#### What is a tree?
 - It's an network of Nodes(Contain data) connected with the help of Edges 
 - Other terminology related to tree: Parent node, Child node, Root node & Leaf node
-# What is a binary tree?
+#### What is a binary tree?
 There are 3 criteria for a binary tree
 1. At most 2 children/node
 2. Exactly 1 root
 3. Exactly 1 path between root and any node
-# How to represent binary tree programmatically?
+#### How to represent binary tree programmatically?
 Node is represented as an object which contains 3 properties
 - val (value of the node)
 - left (address of the left child)
