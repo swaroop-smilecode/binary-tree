6746caaa6dd32934cbf797b403283ddc17e62959
swaroop <swaroopiit@gmail.com>
2024-11-05 16:36:00 +0530

    Solved some more problems

--- Problems/p15_build_tree_in_pre/p15_build_tree_in_pre_i.py
+++ Problems/p15_build_tree_in_pre/p15_build_tree_in_pre_i.py
@@ -5,21 +5,23 @@ class Node:
         self.right = None
 
 def build_tree_in_pre(in_order, pre_order):
-  return _build_tree_in_pre(in_order, pre_order, 0, len(in_order) - 1, 0, len(pre_order) - 1)
+    return _build_tree_in_pre(in_order, pre_order, 0, len(in_order) - 1, 0, len(pre_order) - 1)
   
 def _build_tree_in_pre(in_order, pre_order, in_start, in_end, pre_start, pre_end):
-  if in_end < in_start:
-    return None
-  value = pre_order[pre_start]
-  root = Node(value)
-  mid = in_order.index(value)
-  left_size = mid - in_start
-  root.left = _build_tree_in_pre(in_order, pre_order, in_start, mid - 1, pre_start + 1, pre_start + left_size)
-  root.right = _build_tree_in_pre(in_order, pre_order, mid + 1, in_end, pre_start + left_size + 1, pre_end)
-  return root
+    if (in_end < in_start) or (pre_end < pre_start):
+        return None
+
+    value = pre_order[pre_start]
+    root = Node(value)
+    mid = in_order.index(value)
+    left_size = mid - in_start
+    root.left = _build_tree_in_pre(in_order, pre_order, in_start, mid - 1, pre_start + 1, pre_start + left_size)
+    root.right = _build_tree_in_pre(in_order, pre_order, mid + 1, in_end, pre_start + left_size + 1, pre_end)
+
+    return root
 
 
-print((
+print(build_tree_in_pre(
   [ 'z', 'y', 'x' ],
   [ 'y', 'z', 'x' ] 
 ))
